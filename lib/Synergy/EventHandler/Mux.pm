e9f29f09b83c2d6a9435c5d48e8e38e27aac6b46
Matthew Horsfall <wolfsage@gmail.com>
2018-03-14 12:11:59 -0400

    Simple crash catching

--- lib/Synergy/EventHandler/Mux.pm
+++ lib/Synergy/EventHandler/Mux.pm
@@ -6,6 +6,7 @@ with 'Synergy::Role::EventHandler';
 
 use experimental qw(signatures);
 use namespace::clean;
+use Try::Tiny;
 
 has event_handlers => (
   isa => 'ArrayRef',
@@ -21,7 +22,17 @@ sub start ($self) {
 
 sub handle_event ($self, $event, $rch) {
   for my $handler ($self->event_handlers) {
-    last if $handler->handle_event($event, $rch);
+    my $last = try {
+      return 1 if $handler->handle_event($event, $rch);
+    } catch {
+      my $error = $_;
+
+      $error =~ s/\n.*//ms;
+
+      $rch->reply("$handler crashed while handling your message ($error). Sorry");
+    };
+
+    last if $last;
   }
 
   return;
