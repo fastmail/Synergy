4f57d94966f3c40b1ee36bae1c4ef289743aa5ff
Matthew Horsfall <wolfsage@gmail.com>
2018-03-14 10:55:41 -0400

    Add SlackID event handler

---
+++ lib/Synergy/EventHandler/SlackID.pm
@@ -0,0 +1,39 @@
+use v5.24.0;
+package Synergy::EventHandler::SlackID;
+
+use Moose;
+with 'Synergy::Role::EventHandler';
+
+use experimental qw(signatures);
+use namespace::clean;
+use List::Util qw(first);
+
+my $OWN_NAME = $ENV{SYNERGY_NAME};
+
+sub start { }
+
+sub handle_event ($self, $event, $rch) {
+  return unless $event->type eq 'message';
+
+  return unless $rch->channel->can('slack');
+
+  return unless $event->text =~ /^\@?$OWN_NAME\W/;
+
+  return unless $event->text =~ /slackid (\w+)/;
+
+  my $who = $1;
+
+  my $user = first { $_->{name} eq $who } values $rch->channel->slack->users->%*;
+
+  unless ($user) {
+    $rch->reply("Sorry, I don't know who $who is");
+
+    return 1;
+  }
+
+  $rch->reply("Their slack ID is $user->{id}");
+
+  return 1;
+}
+
+1;
