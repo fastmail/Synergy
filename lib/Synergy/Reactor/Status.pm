cc3b6e778185c82f9fae3d5035c418b79abc1248
Matthew Horsfall <wolfsage@gmail.com>
2018-03-16 14:47:20 -0400

    Add status reactor

---
+++ lib/Synergy/Reactor/Status.pm
@@ -0,0 +1,28 @@
+use v5.24.0;
+package Synergy::Reactor::Status;
+
+use Moose;
+use DateTime;
+with 'Synergy::Role::Reactor';
+
+use experimental qw(signatures);
+use namespace::clean;
+use List::Util qw(first);
+use Time::Duration::Parse;
+use Time::Duration;
+
+sub listener_specs {
+  return {
+    name      => 'status',
+    method    => 'handle_status',
+    exclusive => 1,
+    predicate => sub ($self, $e) { $e->was_targeted && $e->text =~ /^status(\s|$)/i },
+  };
+}
+
+sub handle_status ($self, $event, $rch) {
+  my $uptime = duration(time - $^T);
+  return $rch->reply("Online for $uptime.");
+}
+
+1;
