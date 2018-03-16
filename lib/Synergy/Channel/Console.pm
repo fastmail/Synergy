1eb0b2701eaf904c049e99e12bb31269819fa366
Ricardo Signes <rjbs@cpan.org>
2018-03-16 17:01:28 -0400

    Console channel: for testing on an airplane

---
+++ lib/Synergy/Channel/Console.pm
@@ -0,0 +1,91 @@
+use v5.24.0;
+package Synergy::Channel::Console;
+
+use Moose;
+use experimental qw(signatures);
+use JSON::MaybeXS;
+
+use Synergy::External::Slack;
+use Synergy::Event;
+use Synergy::ReplyChannel;
+use Synergy::Logger '$Logger';
+
+use namespace::autoclean;
+use Data::Dumper::Concise;
+
+with 'Synergy::Role::Channel';
+
+has from_address => (
+  is  => 'ro',
+  isa => 'Str',
+  default => 'sysop',
+);
+
+has stream => (
+  reader    => '_stream',
+  init_arg  => undef,
+  default   => sub {
+    my ($channel) = @_;
+    Scalar::Util::weaken($channel);
+
+    return IO::Async::Stream->new(
+      read_handle  => \*STDIN,
+      write_handle => \*STDOUT,
+
+      on_read => sub {
+        my ( $self, $buffref, $eof ) = @_;
+
+         while( $$buffref =~ s/^(.*\n)// ) {
+            my $text = $1;
+            chomp $text;
+
+            my $user = $channel->hub->user_directory->user_by_channel_and_address(
+              $channel,
+              $channel->from_address,
+            );
+
+            my $evt = Synergy::Event->new({
+              type => 'message',
+              text => $text,
+              was_targeted  => 1,
+              is_public     => 0,
+              from_channel  => $channel,
+              from_address  => $channel->from_address,
+              ( $user ? ( from_user => $user ) : () ),
+              transport_data => $text,
+            });
+
+            my $rch = Synergy::ReplyChannel->new(
+              channel => $channel,
+              prefix  => ($user ? $user->username : $channel->from_address) . ": ",
+              default_address => $channel->from_address,
+              private_address => $channel->from_address,
+            );
+
+            $channel->hub->handle_event($evt, $rch);
+         }
+
+         return 0;
+      }
+    );
+  },
+);
+
+sub start ($self) {
+  $self->hub->loop->add($self->_stream);
+}
+
+sub send_message_to_user ($self, $user, $text) {
+  $self->send_text($user->username, $text);
+}
+
+sub send_text ($self, $address, $text) {
+  $self->_stream->write(">>> $address > $text\n");
+  return;
+}
+
+sub describe_event ($self, $event) {
+  return "(a console event)";
+}
+
+1;
