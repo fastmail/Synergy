b5a8195b3bcc4b5a27f5050ed0ecca1ffb97e59a
Michael McClimon <michael@mcclimon.org>
2018-04-16 16:12:54 -0400

    Add 'eject warp core'

---
+++ lib/Synergy/Reactor/Eject.pm
@@ -0,0 +1,39 @@
+use v5.24.0;
+package Synergy::Reactor::Eject;
+
+use Moose;
+use DateTime;
+with 'Synergy::Role::Reactor';
+
+use experimental qw(signatures);
+use namespace::clean;
+use List::Util qw(first uniq);
+use IO::Async::Timer::Countdown;
+
+sub listener_specs {
+  return {
+    name      => 'warp-core',
+    method    => 'handle_eject',
+    exclusive => 1,
+    predicate => sub ($self, $e) { $e->was_targeted && $e->text eq 'eject warp core' },
+  };
+}
+
+sub handle_eject ($self, $event, $rch) {
+  $event->mark_handled;
+  return $rch->reply('only the master user can do that')
+    unless $event->from_user && $event->from_user->is_master;
+
+  $rch->reply('Good bye.');
+
+  my $timer = IO::Async::Timer::Countdown->new(
+    delay => 1,
+    on_expire => sub { kill 'INT', $$ },
+  );
+
+  $timer->start;
+
+  $self->hub->loop->add($timer);
+}
+
+1;
