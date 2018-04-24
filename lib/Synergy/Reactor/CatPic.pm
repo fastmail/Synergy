9279c560190463d20f4e2f432ac95c7c3c0b63b0
Ricardo Signes <rjbs@cpan.org>
2018-04-23 21:00:51 -0400

    cat pics: also dogs pics

--- lib/Synergy/Reactor/CatPic.pm
+++ lib/Synergy/Reactor/CatPic.pm
@@ -5,18 +5,30 @@ use Moose;
 use DateTime;
 with 'Synergy::Role::Reactor';
 
+use Synergy::Logger '$Logger';
+
 use experimental qw(signatures);
 use namespace::clean;
 
 sub listener_specs {
-  return {
-    name      => 'cat-pic',
-    method    => 'handle_cat_pic',
-    exclusive => 1,
-    predicate => sub ($self, $e) {
-      $e->was_targeted && $e->text =~ /\Acat\s+(pic|jpg|gif|png)\z/
+  return (
+    {
+      name      => 'dog-pic',
+      method    => 'handle_dog_pic',
+      exclusive => 1,
+      predicate => sub ($self, $e) {
+        $e->was_targeted && $e->text =~ /\Adog\s+pic\z/
+      },
     },
-  };
+    {
+      name      => 'cat-pic',
+      method    => 'handle_cat_pic',
+      exclusive => 1,
+      predicate => sub ($self, $e) {
+        $e->was_targeted && $e->text =~ /\Acat\s+(pic|jpg|gif|png)\z/
+      },
+    },
+  );
 }
 
 sub handle_cat_pic ($self, $event, $rch) {
@@ -37,6 +49,26 @@ sub handle_cat_pic ($self, $event, $rch) {
   }
 
   $rch->reply("Something went wrong getting the kitties! \N{CRYING CAT FACE}");
+  return;
+}
+
+sub handle_dog_pic ($self, $event, $rch) {
+  $event->mark_handled;
+
+  my $res = $self->hub->http->GET(
+    "http://dog.ceo/api/breeds/image/random",
+  )->get;
+
+  my $json = eval { JSON::MaybeXS->new->decode( $res->decoded_content ) };
+  my $error = $@;
+
+  if ($json && $json->{status} eq 'success') {
+    $rch->reply($json->{message});
+    return;
+  }
+
+  $Logger->log("doggo error: $error") if $error;
+  $rch->reply("Something went wrong getting the doggos!");
   return;
 }
 
