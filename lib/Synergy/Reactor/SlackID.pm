40efea2f739cac51e798a7038b58926a8e30112c
Michael McClimon <michael@mcclimon.org>
2018-05-01 09:25:38 -0400

    Add command to reload slack users/channels
    Also this makes 'slackid @person' work (with at sign).

--- lib/Synergy/Reactor/SlackID.pm
+++ lib/Synergy/Reactor/SlackID.pm
@@ -9,27 +9,41 @@ use namespace::clean;
 use List::Util qw(first);
 
 sub listener_specs {
-  return {
-    name      => 'slackid',
-    method    => 'handle_event',
-    predicate => sub ($self, $event) {
-      return unless $event->type eq 'message';
+  return (
+    {
+      name      => 'slackid',
+      method    => 'handle_event',
+      predicate => sub ($self, $event) {
+        return unless $event->type eq 'message';
 
-      return unless $event->from_channel->can('slack');
+        return unless $event->from_channel->can('slack');
 
-      return unless $event->was_targeted;
+        return unless $event->was_targeted;
 
-      return unless $event->text =~ /slackid (\w+)/;
+        return unless $event->text =~ /slackid \@?(\w+)/;
 
-      return 1;
+        return 1;
+      },
     },
-  };
+    {
+      name      => 'reload-slack-users',
+      method    => 'handle_reload_slack',
+      predicate => sub ($self, $event) {
+        return unless $event->type eq 'message';
+        return unless $event->was_targeted;
+        return unless $event->from_channel->can('slack');
+
+        return unless $event->text =~ /^reload slack (users|channels)/in;
+        return 1;
+      },
+    },
+  );
 }
 
 sub handle_event ($self, $event, $rch) {
   $event->mark_handled;
 
-  $event->text =~ /slackid (\w+)/;
+  $event->text =~ /slackid \@?(\w+)/;
 
   my $who = $1;
 
@@ -42,6 +56,26 @@ sub handle_event ($self, $event, $rch) {
   }
 
   $rch->reply("Their slack ID is $user->{id}");
+}
+
+sub handle_reload_slack ($self, $event, $rch) {
+  my ($what) = $event->text =~ /^reload slack (users|channels)/i;
+
+  if ($what eq 'users') {
+    $event->mark_handled;
+    $event->from_channel->slack->load_users;
+    $event->from_channel->slack->load_dm_channels;
+    return $rch->reply('Slack users reloaded');
+  }
+
+  if ($what eq 'channels') {
+    $event->mark_handled;
+    $event->from_channel->slack->load_channels;
+    return $rch->reply('Slack channels reloaded');
+  }
+
+  # Don't mark handled, will fall back to "does not compute"
+  return -1;
 }
 
 1;
