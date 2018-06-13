c2594bdca7434e743f345c0bcc24f6fe55e32f54
Ricardo Signes <rjbs@cpan.org>
2018-06-13 15:43:16 -0400

    LiquidPlanner: try a less conspicuous task icon

--- lib/Synergy/Reactor/LiquidPlanner.pm
+++ lib/Synergy/Reactor/LiquidPlanner.pm
@@ -219,7 +219,7 @@ sub provide_lp_link ($self, $event, $rch) {
     my $reply;
 
     if ($item->{type} =~ /\A Task | Package | Project \z/x) {
-      my $icon = $item->{type} eq 'Task'    ? "⏺"
+      my $icon = $item->{type} eq 'Task'    ? "•"
                : $item->{type} eq 'Package' ? "📦"
                : $item->{type} eq 'Project' ? "📁"
                :                              "($item->{type})";
