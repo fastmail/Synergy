e4ccd3a463238e4205b7858eb2a9f67eb5fedaaa
Ricardo Signes <rjbs@cpan.org>
2018-06-14 09:46:14 -0400

    SlackID: handle channels

--- lib/Synergy/Reactor/SlackID.pm
+++ lib/Synergy/Reactor/SlackID.pm
@@ -20,7 +20,7 @@ sub listener_specs {
 
         return unless $event->was_targeted;
 
-        return unless $event->text =~ /slackid \@?(\w+)/;
+        return unless $event->text =~ /\A\s*slackid [@#]?(\w+)\s*\z/;
 
         return 1;
       },
@@ -33,7 +33,7 @@ sub listener_specs {
         return unless $event->was_targeted;
         return unless $event->from_channel->can('slack');
 
-        return unless $event->text =~ /^reload slack (users|channels)/in;
+        return unless $event->text =~ /\Areload slack (users|channels)\z/in;
         return 1;
       },
     },
@@ -43,19 +43,28 @@ sub listener_specs {
 sub handle_event ($self, $event, $rch) {
   $event->mark_handled;
 
-  $event->text =~ /slackid \@?(\w+)/;
+  if ($event->text =~ /slackid \@?(\w+)/) {
+    my $who = $1;
+    my $user = first { $_->{name} eq $who }
+               values $rch->channel->slack->users->%*;
 
-  my $who = $1;
+    return $rch->reply("Sorry, I don't know who $who is")
+      unless $user;
 
-  my $user = first { $_->{name} eq $who } values $rch->channel->slack->users->%*;
-
-  unless ($user) {
-    $rch->reply("Sorry, I don't know who $who is");
-
-    return 1;
+    return $rch->reply("The Slack id for $who is $user->{id}");
   }
 
-  $rch->reply("Their slack ID is $user->{id}");
+  if ($event->text =~ /slackid #(\w+)/) {
+    my $ch_name = $1;
+    my $channel = $rch->channel->slack->channel_named($ch_name);
+
+    return $rch->reply("Sorry, I can't find #$ch_name.")
+      unless $channel;
+
+    return $rch->reply("The Slack id for #$ch_name is $channel->{id}");
+  }
+
+  return $rch->reply(qq{Sorry, I don't know how to resolve that.});
 }
 
 sub handle_reload_slack ($self, $event, $rch) {
