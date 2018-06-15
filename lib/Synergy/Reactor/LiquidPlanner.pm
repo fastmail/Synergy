2db2c95a1aeda723558c8567e744337e8f4a5276
Ricardo Signes <rjbs@cpan.org>
2018-06-15 18:25:39 -0400

    LiquidPlanner: mailbox is better for inbox than inbox

--- lib/Synergy/Reactor/LiquidPlanner.pm
+++ lib/Synergy/Reactor/LiquidPlanner.pm
@@ -2032,8 +2032,8 @@ sub damage_report ($self, $event, $rch) {
   my $lp_id = $target->lp_id;
 
   my @to_check = (
-    [ inbox  => "\N{INBOX TRAY}" => $CONFIG->{liquidplanner}{package}{inbox}  ],
-    [ urgent => "\N{FIRE}"       => $CONFIG->{liquidplanner}{package}{urgent} ],
+    [ inbox  => "📫" => $CONFIG->{liquidplanner}{package}{inbox}  ],
+    [ urgent => "🔥" => $CONFIG->{liquidplanner}{package}{urgent} ],
   );
 
   if ($event->is_public) {
