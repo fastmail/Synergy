f4ba10ed4536171b871582d8410f03963b6b366b
Michael McClimon <michael@mcclimon.org>
2018-06-25 15:20:36 -0400

    Add "emit" reactor
    Useful for testing link formatting.

---
+++ lib/Synergy/Reactor/Emit.pm
@@ -0,0 +1,29 @@
+use v5.24.0;
+package Synergy::Reactor::Emit;
+
+use Moose;
+use DateTime;
+with 'Synergy::Role::Reactor';
+
+use experimental qw(signatures);
+use namespace::clean;
+
+sub listener_specs {
+  return {
+    name      => 'emit',
+    method    => 'handle_emit',
+    exclusive => 1,
+    predicate => sub ($self, $e) {
+      return unless $e->was_targeted;
+      return unless $e->text =~ /\Aemit(?:\s+.+)?/; },
+  };
+}
+
+sub handle_emit ($self, $event, $rch) {
+  my (undef, $text) = split /\s+/, $event->text, 2;
+
+  $rch->reply("$text", { slack => "$text" });
+  $event->mark_handled;
+}
+
+1;
