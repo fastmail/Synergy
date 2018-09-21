5a4a99dc203f1f9571de69e094fc6436c09009d8
Ricardo Signes <rjbs@cpan.org>
2018-09-21 10:52:40 -0400

    no threads, please!

---
+++ lib/Synergy/Reactor/NoThreadsPlease.pm
@@ -0,0 +1,51 @@
+use v5.24.0;
+package Synergy::Reactor::NoThreadsPlease;
+
+use Moose;
+use DateTime;
+with 'Synergy::Role::Reactor';
+
+use experimental qw(signatures);
+use namespace::clean;
+use List::Util qw(first uniq);
+use Synergy::Util qw(parse_date_for_user);
+
+sub listener_specs {
+  return {
+    name      => 'no_threads_please',
+    method    => 'handle_thread',
+    exclusive => 1,
+    predicate => sub ($self, $e) {
+      return unless $e->from_channel->isa('Synergy::Channel::Slack');
+      my $td = $e->transport_data;
+      return unless $td->{thread_ts} && $td->{thread_ts} ne $td->{ts};
+      return 1;
+    },
+  };
+}
+
+has recent_threads => (
+  is  => 'ro',
+  default => sub {  []  },
+);
+
+sub handle_thread ($self, $event) {
+  my $time_ago = time - 1800;
+  $self->recent_threads->@* = grep {; $_->{at} < $time_ago }
+                              $self->recent_threads->@*;
+
+  return if grep {; $_->{who} eq $event->from_address
+                 && $_->{thread} eq $event->transport_data->{thread_ts}
+                 } $self->recent_threads->@*;
+
+  push $self->recent_threads->@*, {
+    at  => time,
+    who => $event->from_address,
+    thread => $event->transport_data->{thread_ts},
+  };
+
+  $event->private_reply("Please don't use threads.  They're just the worst.");
+  return;
+}
+
+1;
