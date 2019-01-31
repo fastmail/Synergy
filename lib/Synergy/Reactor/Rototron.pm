d56b870e2462c89aab898d41035c5edaf6bce4f3
Ricardo Signes <rjbs@cpan.org>
2019-01-31 16:01:57 -0500

    Rototron: add "unavailable" command

--- lib/Synergy/Reactor/Rototron.pm
+++ lib/Synergy/Reactor/Rototron.pm
@@ -11,16 +11,26 @@ use namespace::clean;
 
 use JMAP::Tester;
 use JSON::MaybeXS;
+use Lingua::EN::Inflect qw(NUMWORDS PL_N);
 use Synergy::Logger '$Logger';
 use Synergy::Rototron;
+use Synergy::Util qw(parse_date_for_user);
 
 sub listener_specs {
-  return {
-    name      => 'duty',
-    method    => 'handle_duty',
-    exclusive => 1,
-    predicate => sub ($self, $e) { $e->was_targeted && $e->text =~ /^duty$/i },
-  };
+  return (
+    {
+      name      => 'duty',
+      method    => 'handle_duty',
+      exclusive => 1,
+      predicate => sub ($self, $e) { $e->was_targeted && $e->text =~ /^duty$/i },
+    },
+    {
+      name      => 'unavailable',
+      method    => 'handle_unavailable',
+      exclusive => 1,
+      predicate => sub ($self, $e) { $e->was_targeted && $e->text =~ /^unavailable\b/i },
+    },
+  );
 }
 
 has roto_config_path => (
@@ -70,6 +80,55 @@ has jmap_client => (
 after register_with_hub => sub ($self, @) {
   $self->roto_config; # crash early, crash often -- rjbs, 2019-01-31
 };
+
+sub handle_unavailable ($self, $event) {
+  $event->mark_handled;
+
+  my ($from, $to);
+  my $ymd_re = qr{ ([0-9]{4}) - ([0-9]{2}) - ([0-9]{2}) }x;
+
+  my $text = $event->text =~ s/\Aunavailable\b//r;
+  $text =~ s/\A\s+//;
+
+  if ($text =~ m{\Aon\s+($ymd_re)\z}) {
+    $from = parse_date_for_user("$1", $event->from_user);
+    $to   = $from->clone;
+  } elsif ($text =~ m{\Afrom\s+($ymd_re)\s+to\s+($ymd_re)\z}) {
+    my ($d1, $d2) = ($1, $2);
+    $from = parse_date_for_user($d1, $event->from_user);
+    $to   = parse_date_for_user($d2, $event->from_user);
+  } else {
+    return $event->reply(
+      "It's: `unavailable on YYYY-MM-DD` "
+      . "or `unavailable from YYYY-MM-DD to YYYY-MM-DD`"
+    );
+  }
+
+  $from->truncate(to => 'day');
+  $to->truncate(to => 'day');
+
+  my @dates;
+  until ($from > $to) {
+    push @dates, $from;
+    $from->add(days => 1);
+  }
+
+  unless (@dates) { return $event->reply("That range didn't make sense."); }
+  if (@dates > 28) { return $event->reply("That range is too large."); }
+
+  for my $date (@dates) {
+    $self->availability_checker->set_user_unavailable_on(
+      $event->from_user->username,
+      $date,
+    );
+  }
+
+  return $event->reply(
+    sprintf "I marked you unavailable on %s %s.",
+      NUMWORDS(0+@dates),
+      PL_N('day', 0+@dates),
+  );
+}
 
 sub handle_duty ($self, $event) {
   $event->mark_handled;
