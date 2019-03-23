ae4446941a2ebb693ca910a750e31f4f68dc6842
Ricardo Signes <rjbs@cpan.org>
2019-03-22 22:02:30 -0400

    HelpSpot: unassigned report

--- lib/Synergy/Reactor/HelpSpot.pm
+++ lib/Synergy/Reactor/HelpSpot.pm
@@ -7,6 +7,7 @@ with 'Synergy::Role::Reactor';
 
 use URI;
 use URI::QueryParam;
+use XML::LibXML;
 
 use experimental qw(signatures);
 use namespace::clean;
@@ -34,7 +35,9 @@ sub _http_get ($self, $param) {
 
   my $res = $self->hub->http_get(
     $uri,
+    async => 1,
     Authorization => 'Basic ' . $self->auth_token,
+    'User-Agent'  => __PACKAGE__,
   );
 }
 
@@ -42,5 +45,33 @@ sub helpspot_report ($self, $who) {
   return Future->done([ "not yet implemented" ]);
 }
 
+sub unassigned_report ($self, $who, $arg = {}) {
+  $self->_http_get({
+    method => 'private.request.search',
+    ($arg->{search_args} ? $arg->{search_args}->%* : ()),
+  })->then(sub ($res) {
+    open my $fh, '<', \$res->decoded_content(charset => 'none')
+      or die "error making handle to XML results: $!";
+
+    my $doc = XML::LibXML->load_xml(IO => $fh);
+
+    my @requests = $doc->getElementsByTagName('request');
+
+    my $count = 0;
+    for my $request (@requests) {
+      my ($person) = $request->getElementsByTagName('xPersonAssignedTo');
+      next if $person && $person->textContent;
+      $count++;
+    }
+
+    return Future->done unless $count;
+
+    my $desc = $arg->{description} // "tickets";
+    return Future->done([
+      "\N{BUG} Unassigned $desc: $count",
+      { slack => "\N{BUG} Unassigned $desc: $count" },
+    ]);
+  });
+}
 
 1;
