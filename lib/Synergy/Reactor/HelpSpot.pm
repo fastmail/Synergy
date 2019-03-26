3af3ee7329b14a809f41f01c7bf03bd5825e3bcc
Ricardo Signes <rjbs@cpan.org>
2019-03-26 16:50:26 -0400

    HelpSpot: add hacky triage-only flag to report
    It should really be a reusable behavior

--- lib/Synergy/Reactor/HelpSpot.pm
+++ lib/Synergy/Reactor/HelpSpot.pm
@@ -46,6 +46,8 @@ sub helpspot_report ($self, $who) {
 }
 
 sub unassigned_report ($self, $who, $arg = {}) {
+  return if $arg->{triage_only} && ! $who->is_on_triage;
+
   $self->_http_get({
     method => 'private.request.search',
     ($arg->{search_args} ? $arg->{search_args}->%* : ()),
