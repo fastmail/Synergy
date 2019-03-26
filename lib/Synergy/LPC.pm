bbdf608848c7fa41067591ac8ef1378efda47471
Ricardo Signes <rjbs@cpan.org>
2019-03-26 16:29:01 -0400

    WIP: lpc with futures

--- lib/Synergy/LPC.pm
+++ lib/Synergy/LPC.pm
@@ -41,24 +41,6 @@ sub _lp_base_uri ($self) {
   return "https://app.liquidplanner.com/api/workspaces/" . $self->workspace_id;
 }
 
-# Yes, this means that even though an undef payload is technically possible, we
-# turn it into a nil result.  If you really need a non-nil undef, construct one
-# by hand.  But you probably don't. -- rjbs, 2018-06-26
-my sub _failure { state $fail = LPC::Result::Failure->new; return $fail }
-my sub _success ($payload) {
-  return (defined $payload) ? LPC::Result::Success->new({ payload => $_[0] })
-                            : LPC::Result::Success->new;
-}
-
-sub _http_failure ($self, $http_res, $desc = undef) {
-  $self->log([
-    "error with %s: %s",
-    $desc // "HTTP operation",
-    $http_res->as_string,
-  ]);
-  return _failure;
-}
-
 my $CONFIG;  # XXX use real config
 
 $CONFIG = {
@@ -99,45 +81,52 @@ has http_post_callback => (
 sub http_get ($self, $path, @arg) {
   my $uri = $self->_lp_base_uri . $path;
 
-  my $http_res = $self->http_get_raw(
+  my $res_f = $self->http_get_raw(
     $uri,
     @arg,
+    async => 1,
     Authorization => $self->auth_token,
   );
 
-  return $self->_http_failure($http_res) unless $http_res->is_success;
+  $res_f->then(sub ($res) {
+    $self->log([
+      "error with GET $uri: %s",
+      $res->as_string,
+    ]);
 
-  my $payload = $JSON->decode($http_res->decoded_content);
-  return _success($payload);
+    return Future->fail($res) unless $res->is_success;
+    return Future->done($JSON->decode($res->decoded_content));
+  });
 }
 
 sub http_post ($self, $path, @arg) {
   my $uri = $self->_lp_base_uri . $path;
 
-  my $http_res = $self->http_post_raw(
+  my $res_f = $self->http_post_raw(
     $uri,
     @arg,
+    async => 1,
     Authorization => $self->auth_token,
   );
 
-  return $self->_http_failure($http_res) unless $http_res->is_success;
+  $res_f->then(sub ($res) {
+    $self->log([
+      "error with GET $uri: %s",
+      $res->as_string,
+    ]);
 
-  my $payload = $JSON->decode($http_res->decoded_content);
-  return _success($payload);
+    return Future->fail($res) unless $res->is_success;
+    return Future->done($JSON->decode($res->decoded_content));
+  });
 }
 
 sub get_clients ($self) {
-  my $lp_res = $self->http_get("/clients");
-
-  return $lp_res unless $lp_res->is_success;
-  return _success($lp_res->payload);
+  $self->http_get("/clients");
 }
 
 sub get_item ($self, $item_id) {
-  my $lp_res = $self->http_get("/treeitems/?filter[]=id=$item_id");
-
-  return $lp_res unless $lp_res->is_success;
-  return _success($lp_res->payload->[0]);
+  $self->http_get("/treeitems/?filter[]=id=$item_id")
+       ->then(sub ($data) { $data->[0] });
 }
 
 has single_activity_id => (
@@ -147,8 +136,9 @@ has single_activity_id => (
 );
 
 sub get_activity_id ($self, $task_or_id, $member_id = undef) {
-  return _success($self->single_activity_id) if $self->has_single_activity_id;
-  return _failure("get_activity_id not really implemented");
+  return $self->has_single_activity_id
+         ? Future->done($self->single_activity_id)
+         : Future->fail("get_activity_id not really implemented");
 
   # my $task_res = $lpc->get_item($lp_timer->{item_id});
 
@@ -167,14 +157,11 @@ sub my_timers ($self) {
 
 sub my_running_timer ($self) {
   # Treat as impossible, for now, >1 running timer. -- rjbs, 2018-06-26
-  my $timer_res = $self->my_timers;
-  return $timer_res unless $timer_res->is_success;
-
-  my ($timer) = grep {; $_->{running} } $timer_res->payload_list;
-  if ($timer) {
-    $timer = LPC::Timer->new($timer);
-  }
-  return _success($timer);
+  $self->my_timers->then(sub ($data) {
+    my ($timer) = grep {; $_->{running} } @$data;
+    return $timer ? Future->done( LPC::Timer->new($timer) )
+                  : Future->done;
+  });
 }
 
 sub query_items ($self, $arg) {
@@ -200,9 +187,10 @@ sub upcoming_task_groups_for_member_id ($self, $member_id, $limit = 200) {
 
 sub start_timer_for_task_id ($self, $task_id) {
   my $start_res = $self->http_post("/tasks/$task_id/timer/start");
-  return $start_res unless $start_res->is_success;
-  return _failure unless $start_res->payload->{running}; # Really??
-  return $start_res;
+  $start_res->then_with_f(sub ($f, $data) {
+    return Future->fail("new timer not running?!") unless $data->{running};
+    return $f;
+  });
 }
 
 sub stop_timer_for_task_id ($self, $task_id) {
@@ -230,7 +218,7 @@ sub track_time ($self, $arg) {
     $finder->find(\$comment);
   }
 
-  my $res = $self->http_post(
+  my $res_f = $self->http_post(
     "/tasks/$arg->{task_id}/track_time",
     Content_Type => 'application/json',
     Content => $JSON->encode({
@@ -243,34 +231,32 @@ sub track_time ($self, $arg) {
     }),
   );
 
-  return $res unless $res->is_success;
+  $res_f->then_with_f(sub ($f, $task) {
+    $self->log([ "response from track_time: %s", $task ]);
 
-  my $task = $res->payload;
+    my ($assignment) = grep {; $_->{person_id} == $arg->{member_id} }
+                       $task->{assignments}->@*;
 
-  $self->log([ "response from track_time: %s", $res->payload ]);
+    die "WHERE IS MY ASSIGNMENT" unless $assignment;
 
-  my ($assignment) = grep {; $_->{person_id} == $arg->{member_id} }
-                     $task->{assignments}->@*;
+    if ($arg->{done} xor $assignment->{is_done}) {
+      $f = $f->then(
+        "/tasks/$arg->{task_id}/update_assignment",
+        Content_Type => 'application/json',
+        Content => $JSON->encode({
+          assignment_id => $assignment->{id},
+          is_done       => ($arg->{done} ? \1 : \0),
+        }),
+      );
+    }
 
-  die "WHERE IS MY ASSIGNMENT" unless $assignment;
-
-  if ($arg->{done} xor $assignment->{is_done}) {
-    my $assignment_res = $self->http_post(
-      "/tasks/$arg->{task_id}/update_assignment",
-      Content_Type => 'application/json',
-      Content => $JSON->encode({
+    return $f->then(sub {
+      Future->done({
+        item_id => $arg->{task_id},
+        work    => $arg->{work},
         assignment_id => $assignment->{id},
-        is_done       => ($arg->{done} ? \1 : \0),
-      }),
-    );
-
-    return $assignment_res unless $assignment_res->is_success;
-  }
-
-  return _success({
-    item_id => $arg->{task_id},
-    work    => $arg->{work},
-    assignment_id => $assignment->{id},
+      });
+    });
   });
 }
 
@@ -299,42 +285,42 @@ sub create_todo_item ($self, $todo) {
 sub current_iteration ($self) {
   my $helper = LPC::IterationHelper->new({ lpc => $self });
 
-  my $iter    = $helper->current_iteration;
-  my $pkg_res = $helper->package_for_iteration_number($iter->{number});
+  my $iter  = $helper->current_iteration;
+  my $pkg_f = $helper->package_for_iteration_number($iter->{number});
 
-  return unless $pkg_res->is_success;
+  return unless $pkg_f->await->is_done;
 
   return {
     %$iter,
-    package => $pkg_res->payload,
+    package => $pkg_f->get,
   };
 }
 
 sub iteration_by_number ($self, $n) {
   my $helper = LPC::IterationHelper->new({ lpc => $self });
 
-  my $iter    = $helper->iteration_by_number($n);
-  my $pkg_res = $helper->package_for_iteration_number($iter->{number});
+  my $iter  = $helper->iteration_by_number($n);
+  my $pkg_f = $helper->package_for_iteration_number($iter->{number});
 
-  return unless $pkg_res->is_success;
+  return unless $pkg_f->await->is_done;
 
   return {
     %$iter,
-    package => $pkg_res->payload,
+    package => $pkg_f->get,
   };
 }
 
 sub iteration_relative_to_current ($self, $delta_n) {
   my $helper = LPC::IterationHelper->new({ lpc => $self });
 
-  my $iter    = $helper->iteration_relative_to_current($delta_n);
-  my $pkg_res = $helper->package_for_iteration_number($iter->{number});
+  my $iter  = $helper->iteration_relative_to_current($delta_n);
+  my $pkg_f = $helper->package_for_iteration_number($iter->{number});
 
-  return unless $pkg_res->is_success;
+  return unless $pkg_f->is_success;
 
   return {
     %$iter,
-    package => $pkg_res->payload,
+    package => $pkg_f->get,
   };
 }
 
@@ -507,7 +493,7 @@ package LPC::IterationHelper {
     # We could cache this, but the lifecycle of IterationHelper and LPC objects
     # is not really reliable and we'd end up with duplicates and it would be
     # stupid. -- rjbs, 2019-02-27
-    my $res = $self->lpc->query_items({
+    my $query_f = $self->lpc->query_items({
       # in => $root_id
       flags => {
         depth => -1,
@@ -518,9 +504,7 @@ package LPC::IterationHelper {
       ],
     });
 
-    return $res unless $res->is_success;
-
-    return _success($res->payload->[0]);
+    return $query_f->then(sub ($data) { $data->[0] });
   }
 
   sub package_for_current_iteration ($self) {
