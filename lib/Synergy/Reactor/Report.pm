5f55cfc3efd3da4d5ae4debded87c88825fb4e97
Ricardo Signes <rjbs@cpan.org>
2019-10-30 11:24:15 -0400

    Report: fix name of alternatives

--- lib/Synergy/Reactor/Report.pm
+++ lib/Synergy/Reactor/Report.pm
@@ -60,7 +60,7 @@ after register_with_hub => sub ($self, @) {
 sub fixed_text_report ($self, $who, $arg = {}) {
   return Future->done([
     $arg->{text},
-    $arg->{alt},
+    $arg->{alts},
   ]);
 }
 
