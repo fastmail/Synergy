0a4ab27b6392f8c60ff176a462c388e0aa8c38b1
Ricardo Signes <rjbs@cpan.org>
2020-01-07 21:07:31 -0500

    VicEPA: add forecast retrieval

--- lib/Synergy/Reactor/VicEPA.pm
+++ lib/Synergy/Reactor/VicEPA.pm
@@ -6,10 +6,13 @@ use Moose;
 use DateTime;
 with 'Synergy::Role::Reactor::EasyListening';
 
+use utf8;
 use experimental qw(signatures lexical_subs);
 use namespace::clean;
-use List::Util qw(first uniq);
+use List::Util qw(uniq);
 use Synergy::Util qw(parse_date_for_user);
+
+use Synergy::Logger '$Logger';
 
 has api_key => (
   is => 'ro',
@@ -20,6 +23,17 @@ has site_id => (
   is => 'ro',
   required => 1,
 );
+
+has latitude => (
+  is => 'ro',
+  required => 1,
+);
+
+has longitude => (
+  is => 'ro',
+  required => 1,
+);
+
 
 sub listener_specs {
   return {
@@ -42,46 +56,99 @@ sub handle_airwatch ($self, $event) {
   my $now = DateTime->now(time_zone => 'UTC');
   my $ago = $now->clone->subtract(hours => 12);
 
-  my $url = sprintf 'https://gateway.api.epa.vic.gov.au/environmentMonitoring/v1/sites/%s/parameters?since=%s&until=%s',
-    $self->site_id,
-    $ago->strftime('%FT%TZ'),
-    $now->strftime('%FT%TZ');
-
-  my $http_future = $self->hub->http_client->GET(
-    $url,
+  my $seen = $self->hub->http_client->GET(
+    sprintf(
+      'https://gateway.api.epa.vic.gov.au/environmentMonitoring/v1/sites/%s/parameters?since=%s&until=%s',
+      $self->site_id,
+      $ago->strftime('%FT%TZ'),
+      $now->strftime('%FT%TZ'),
+    ),
     headers => [
       'X-API-Key' => $self->api_key,
     ],
   );
 
-  $http_future
-    ->then(sub ($res) {
-      my $report  = JSON::MaybeXS->new->decode($res->decoded_content);
+  my $forecast = $self->hub->http_client->GET(
+    sprintf(
+      'https://gateway.api.epa.vic.gov.au/environmentMonitoring/v1/forecasts/?environmentalSegment=%s&location=%s,%s',
+      'air',
+      $self->latitude,
+      $self->longitude,
+    ),
+    headers => [
+      'X-API-Key' => $self->api_key,
+    ],
+  );
+
+  Future->wait_all($seen, $forecast)
+    ->then(sub {
+      my $report  = JSON::MaybeXS->new->decode($seen->get->decoded_content);
       my $where   = $report->{siteName};
       my @by_time = sort { $a->{since} cmp $b->{since} }
                     $report->{siteHealthAdvices}->@*;
-      my @by_val  = sort { $a->{value} <=> $b->{value} }
+      my @by_val  = sort { $a->{averageValue} <=> $b->{averageValue} }
                     $report->{siteHealthAdvices}->@*;
 
-      unless (@by_time) {
-        $event->reply("I couldn't get any measurements in $where for the last 12 hours!");
-        return Future->done;
+      my $report_str = "I couldn't get any measurements in $where for the last 12 hours!";
+
+      if (@by_time) {
+        if ($by_val[0]{healthAdvice} eq $by_val[-1]{healthAdvice}) {
+          $report_str = "Air quality in $where for the past 12h: $by_val[0]{healthAdvice}";
+        } else {
+          $report_str = sprintf "Air quality in $where for the past 12h: ranged from %s to %s; currently %s.",
+            $by_val[0]{healthAdvice},
+            $by_val[-1]{healthAdvice},
+            $by_time[-1]{healthAdvice};
+        }
       }
 
-      if ($by_val[0]{healthAdvice} eq $by_val[-1]{healthAdvice}) {
-        $event->reply("Air quality in $where for the past 12h: $by_val[0]{healthAdvice}");
-        return Future->done;
+      my $fcast   = JSON::MaybeXS->new->decode($forecast->get->decoded_content);
+      my @records = sort {;
+            $a->{regionName} cmp $b->{regionName}
+        ||  $a->{since}      cmp $b->{since}
+      } $fcast->{records}->@*;
+
+      my $fcast_str = "I couldn't get a forecast for $where.";
+
+      if (@records) {
+        $fcast_str = q{The forecast is:};
+        my %saw_region;
+        for my $record (@records) {
+          next if $saw_region{ $record->{regionName} }++;
+
+          my $since = DateTime::Format::ISO8601->parse_datetime($record->{since})
+                                               ->set_time_zone('Australia/Melbourne');
+          my $until = DateTime::Format::ISO8601->parse_datetime($record->{until})
+                                               ->set_time_zone('Australia/Melbourne');
+
+          my $AU = q{🇦🇺};
+          $fcast_str .= sprintf "\n%s, %s to %s $AU: %s",
+            $record->{regionName},
+            $self->hub->format_friendly_date(
+              $since,
+              {
+                include_time_zone => 0,
+                maybe_omit_day    => 1,
+              },
+            ),
+            $self->hub->format_friendly_date(
+              $until,
+              {
+                include_time_zone => 0,
+                maybe_omit_day    => 1,
+              },
+            ),
+            $record->{title};
+        }
       }
 
-      $event->reply(
-        sprintf "Air quality in $where for the past 12h: ranged from %s to %s; currently %s",
-          $by_val[0]{healthAdvice},
-          $by_val[-1]{healthAdvice},
-          $by_time[-1]{healthAdvice},
-      );
+      $event->reply("$report_str\n$fcast_str");
       return Future->done;
     })
-    ->else(sub (@err) { $event->error_reply("Air quality check failed."); })
+    ->else(sub (@err) {
+      $Logger->log([ "Air quality check failed: %s", \@err ]);
+      $event->error_reply("Air quality check failed.");
+    })
     ->retain;
 }
 
