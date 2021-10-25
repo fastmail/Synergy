b4dfb895e6699e08e13cdedfc36260cd158e0b08
Ricardo Signes <rjbs@semiotic.systems>
2021-10-24 22:08:38 -0400

    Notion: use async web requests

--- lib/Synergy/Reactor/Notion.pm
+++ lib/Synergy/Reactor/Notion.pm
@@ -49,7 +49,7 @@ sub handle_my_projects ($self, $event) {
   my $db_id = $self->project_db_id;
   my $token = $self->api_token;
 
-  my $res = $self->hub->http_post(
+  $self->hub->http_post(
     "https://api.notion.com/v1/databases/$db_id/query",
 
     'User-Agent'      => 'Synergy/2021.05',
@@ -57,48 +57,52 @@ sub handle_my_projects ($self, $event) {
     'Notion-Version'  => '2021-08-16',
     'Content-Type'    => 'application/json',
     Content => "{}", # TODO: filter here for real
-  )->get; # TODO: use sequencing
-
-  my $data  = JSON::MaybeXS->new->utf8->decode( $res->decoded_content(charset => undef) );
-  my @pages = $data->{results}->@*;
-
-  my $reply = q{};
-  my $slack = q{};
-
-  my $email = $event->from_user->username . '@' . $self->username_domain;
-
-  for my $page (@pages) {
-    next if $page->{properties}{'On Hold'}{checkbox};
-
-    my $stage = $page->{properties}{Stage}{select}{name};
-    next if $stage eq 'Done';
-
-    my @people = (
-      $page->{properties}{'Team (Active)'}{people}->@*,
-      $page->{properties}{'Coordinator'}{people}->@*,
+  )->then(sub ($res) {
+    my $data  = JSON::MaybeXS->new->utf8->decode(
+      $res->decoded_content(charset => undef)
     );
+    my @pages = $data->{results}->@*;
 
-    next unless grep {; $_->{person}{email} eq $email } @people;
-    my $title = join q{ - }, map {; $_->{plain_text} } $page->{properties}{Name}{title}->@*;
+    my $reply = q{};
+    my $slack = q{};
 
-    my $emoji = $page->{icon}{emoji} // "\N{FILE FOLDER}";
+    my $email = $event->from_user->username . '@' . $self->username_domain;
 
-    my $safe_title = $title;
-    $safe_title =~ s{[^A-Za-z0-9]}{-}g;
+    for my $page (@pages) {
+      next if $page->{properties}{'On Hold'}{checkbox};
 
-    my $id = $page->{id} =~ s/-//gr;
+      my $stage = $page->{properties}{Stage}{select}{name};
+      next if $stage eq 'Done';
 
-    my $href = "https://www.notion.so/$safe_title-$id";
+      my @people = (
+        $page->{properties}{'Team (Active)'}{people}->@*,
+        $page->{properties}{'Coordinator'}{people}->@*,
+      );
 
-    $reply .= "$emoji $title ($stage)\n";
-    $slack .= sprintf "<%s|%s %s> (%s)\n", $href, $emoji, $title, $stage;
-  }
+      next unless grep {; $_->{person}{email} eq $email } @people;
 
-  unless (length $reply) {
-    return $event->reply("Looks like you've got no projects right now!");
-  }
+      my $title = join q{ - },
+        map {; $_->{plain_text} } $page->{properties}{Name}{title}->@*;
 
-  $event->reply($reply, { slack => $slack });
+      my $emoji = $page->{icon}{emoji} // "\N{FILE FOLDER}";
+
+      my $safe_title = $title;
+      $safe_title =~ s{[^A-Za-z0-9]}{-}g;
+
+      my $id = $page->{id} =~ s/-//gr;
+
+      my $href = "https://www.notion.so/$safe_title-$id";
+
+      $reply .= "$emoji $title ($stage)\n";
+      $slack .= sprintf "<%s|%s %s> (%s)\n", $href, $emoji, $title, $stage;
+    }
+
+    unless (length $reply) {
+      return $event->reply("Looks like you've got no projects right now!");
+    }
+
+    $event->reply($reply, { slack => $slack });
+  })->retain;
 }
 
 1;
