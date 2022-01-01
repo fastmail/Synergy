3a6f48ad5238d2804775d46c2b1ae509c004c66b
Ricardo Signes <rjbs@semiotic.systems>
2022-01-01 12:23:19 -0500

    PotentialReaction: new class, a reaction that might happen
    This will be part of replacing Listeners.  A reactor can reply a number
    of potential reactions, as long as there is no more than a single
    exclusive one.  These are then treated like matching listeners.  This
    moves the matching process into the reactor instead of the listeners.
    In reality, all our current reactors use EasyListening, so the logic
    will live there for now.

---
+++ lib/Synergy/PotentialReaction.pm
@@ -0,0 +1,17 @@
+package Synergy::PotentialReaction;
+use Moose;
+
+has reactor => (is => 'ro', required => 1);
+has is_exclusive => (is => 'ro', required => 1);
+has name    => (is => 'ro', required => 1);
+
+sub description { $_[0]->reactor->name . q{/} . $_[0]->name }
+
+has event_handler => (is => 'ro', required => 1);
+
+sub handle_event {
+  $_[0]->event_handler->($_[1]);
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
