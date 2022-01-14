b88c3e6d0c295a56178e0fcbf1d26bed0ecdd4b8
Ricardo Signes <rjbs@semiotic.systems>
2022-01-14 11:00:01 -0500

    LiquidPlanner: cut it all out

--- lib/Synergy/Timer.pm
+++ lib/Synergy/Timer.pm
@@ -96,32 +96,4 @@ sub is_business_hours {
   return 1;
 }
 
-has last_nag => (
-  is  => 'rw',
-  predicate => 'has_last_nag',
-  clearer   => 'clear_last_nag',
-);
-
-has last_saw_timer => (
-  is => 'rw',
-);
-
-sub last_relevant_nag {
-  my ($self) = @_;
-
-  # If we had nagged, but haven't nagged in 45 minutes, let's start over.
-  # This could happen, for example, if we were nagging at the end of the
-  # business day and now it's morning, or if we were at high-severity nagging
-  # before getting told to chill. -- rjbs, 2014-01-15
-  my $last_nag = $self->last_nag;
-  if ($last_nag and time - $last_nag->{time} > 2700) {
-    warn("It's been >45min since last nag.  Resetting nag state.");
-    # $self->info("It's been >45min since last nag.  Resetting nag state.");
-    $self->clear_last_nag;
-    return undef;
-  }
-
-  return $last_nag;
-}
-
 1;
