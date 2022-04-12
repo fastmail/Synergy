b549b593c2a51bdec8dba629365f90c0adaaca91
Michael McClimon <michael@fastmailteam.com>
2022-04-12 10:46:36 -0400

    Linear: show icon for issues marked to-discuss

--- lib/Synergy/Reactor/Linear.pm
+++ lib/Synergy/Reactor/Linear.pm
@@ -121,6 +121,7 @@ sub _slack_item_link ($self, $issue) {
 }
 
 sub _icon_for_issue ($self, $issue) {
+  return "\N{SPEAKING HEAD IN SILHOUETTE}" if $issue->{state}{name} eq 'To Discuss';
   return '✓' if $issue->{state}{type} =~ /\A(canceled|completed)\z/n;
   return "\N{FIRE}" if $issue->{priority} == 1;
   return "•";
