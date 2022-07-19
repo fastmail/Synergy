a18f91c4ecd5a137e5bf013b6687c89443f4aab2
Ricardo Signes <rjbs@semiotic.systems>
2022-07-19 09:56:06 -0400

    Rototron: do not override default_using
    ...because our override was wrong

--- lib/Synergy/Reactor/Rototron.pm
+++ lib/Synergy/Reactor/Rototron.pm
@@ -259,7 +259,6 @@ sub _replan_range ($self, $from_dt, $to_dt) {
   return unless $plan;
 
   my $res = $self->rototron->jmap_client->request({
-    using       => [ 'urn:ietf:params:jmap:mail', 'https://cyrusimap.org/ns/jmap/calendars', ],
     methodCalls => [
       [ 'CalendarEvent/set' => $plan, ],
     ],
