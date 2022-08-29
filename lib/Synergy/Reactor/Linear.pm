304e4fccd8c839653b0915fcc00de224a3030e9e
Ricardo Signes <rjbs@semiotic.systems>
2022-08-29 20:56:09 +1000

    Linear: flip flop on whether "agenda" shows assigned items

--- lib/Synergy/Reactor/Linear.pm
+++ lib/Synergy/Reactor/Linear.pm
@@ -472,7 +472,7 @@ command agenda => {
                     if ($assignee_id) {
                       return Future->done(assignee => $assignee_id);
                     } else {
-                      return Future->done(team => $team_id, assignee => undef);
+                      return Future->done(team => $team_id);
                     }
                   }
                 })
