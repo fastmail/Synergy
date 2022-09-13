648dc1b685ab83e26a9a77ac6ee3c1b188638397
Rasha Moumneh <38481206+RashaMou@users.noreply.github.com>
2022-09-13 12:22:55 -0400

    Update lib/Synergy/Reactor/Linear.pm
    Co-authored-by: Ricardo Signes <rjbs@users.noreply.github.com>

--- lib/Synergy/Reactor/Linear.pm
+++ lib/Synergy/Reactor/Linear.pm
@@ -416,7 +416,7 @@ command agenda => {
                     }
                   }
                 })
-              : Future->done(assignee => { isMe => {eq => \1} }); 
+              : Future->done(assignee => { isMe => { eq => \1 } }); 
 
     $when->then(sub {
       my (%extra_search) = @_;
