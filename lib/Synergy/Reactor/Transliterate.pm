3d8b15c60706ce02c7f20b4464a2553afeaf26cc
Ricardo Signes <rjbs@semiotic.systems>
2022-10-28 12:03:13 -0400

    Transliterate: convert to CommandPost and async/await

--- lib/Synergy/Reactor/Transliterate.pm
+++ lib/Synergy/Reactor/Transliterate.pm
@@ -3,42 +3,32 @@ use warnings;
 package Synergy::Reactor::Transliterate;
 
 use Moose;
-with 'Synergy::Role::Reactor::EasyListening';
+with 'Synergy::Role::Reactor',
+     'Synergy::Role::Reactor::CommandPost';
 
 use experimental qw(signatures);
 use namespace::clean;
+
+use Future::AsyncAwait;
+use Synergy::CommandPost;
 use Synergy::Util qw(known_alphabets transliterate);
 
-sub listener_specs {
-  return {
-    name      => 'transliterate',
-    method    => 'handle_transliterate',
-    exclusive => 1,
-    targeted  => 1,
-    predicate => sub ($self, $e) { $e->text =~ /\Atransliterate to (\S+):\s+/i },
-    help_entries => [
-      {
-        title => 'transliterate',
-        text  => <<'EOH' =~ s/(\S)\n([^\s•])/$1 $2/rg
-*transliterate to `SCRIPT`: `MESSAGE`*: rewrite a string with a different alphabet
-EOH
-      },
-    ]
-  };
-}
-
-sub handle_transliterate ($self, $event) {
+command transliterate => {
+  help => '*transliterate to `SCRIPT`: `MESSAGE`*: rewrite a string with a different alphabet',
+} => async sub ($self, $event, $rest) {
   $event->mark_handled;
 
-  my ($alphabet, $text) = $event->text =~ /\Atransliterate to (\S+): (.+)\z/i;
+  my ($alphabet, $text) = $rest =~ /\Ato (\S+): (.+)\z/i;
 
-  return $event->error_reply("Sorry, I don't know that alphabet.")
+  return await $event->error_reply("Sorry, I don't know that alphabet.")
+    unless defined $alphabet;
+
+  return await $event->error_reply("Sorry, I don't know that alphabet.")
     unless grep {; lc $_ eq lc $alphabet } known_alphabets;
 
   $text = transliterate($alphabet, $text);
 
-  $event->reply("That's: $text");
-  return;
-}
+  await $event->reply("That's: $text");
+};
 
 1;
