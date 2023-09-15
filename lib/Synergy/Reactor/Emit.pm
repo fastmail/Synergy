04c71f6284e5709afbc9d84df7e62823a558b255
Ricardo Signes <rjbs@semiotic.systems>
2023-09-14 22:38:19 -0400

    Emit: add help

--- lib/Synergy/Reactor/Emit.pm
+++ lib/Synergy/Reactor/Emit.pm
@@ -13,6 +13,7 @@ use Future::AsyncAwait;
 use Synergy::CommandPost;
 
 command emit => {
+  help => "*emit `MESSAGE`*: repeat after me...",
 } => async sub ($self, $event, $rest) {
   $event->mark_handled;
   await $event->reply("$rest", { slack => "$rest" });
