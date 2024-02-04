12b059d5f41022ac0ed2f6382122d8590fc089a8
Ricardo Signes <rjbs@semiotic.systems>
2024-02-04 16:40:34 +0100

    Emit: commands do not need mark_handled

--- lib/Synergy/Reactor/Emit.pm
+++ lib/Synergy/Reactor/Emit.pm
@@ -15,7 +15,6 @@ use Synergy::CommandPost;
 command emit => {
   help => "*emit `MESSAGE`*: repeat after me...",
 } => async sub ($self, $event, $rest) {
-  $event->mark_handled;
   await $event->reply("$rest", { slack => "$rest" });
 };
 
