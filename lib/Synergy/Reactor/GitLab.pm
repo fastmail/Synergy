fastmail/Synergy

commit 98df89e63fe7eaa1502060fe75398a957d455833

GitLab: use hub->http_post, not http_client

lib/Synergy/Reactor/GitLab.pm
@@ -1062,15 +1062,14 @@ async sub mr_report ($self, $who, $arg = {}) {
 }
 
 async sub post_gitlab_snippet ($self, $payload) {
-  my $res = await $self->hub->http_client->do_request(
-    method => 'POST',
-    uri    => $self->api_uri . '/v4/snippets',
+  my $res = await $self->hub->http_post(
+    $self->api_uri . '/v4/snippets',
     headers => {
       'PRIVATE-TOKEN' => $self->api_token,
     },
 
-    content_type => 'application/json',
-    content      => encode_json($payload),
+    Content_Type => 'application/json',
+    Content      => encode_json($payload),
   );
 
   unless ($res->is_success) {

commit 27584b5ed8cc153d7bfa1aaa2ac4face2c202c36

GitLab: Fix post_gitlab_snippet headers
This has been broken since 98df89e when it was changed to use
hub->http_post

Other than Content and Content_Type k:v pairs, hub->http_request()
presumes all other k:v pairs are headers.

By calling with with headers => { foo=> bar }, the actual arguments into
http_client->do_request() were

headers => { headers => { foo => bar } }
instead of
headers => { foo => bar }

lib/Synergy/Reactor/GitLab.pm
@@ -1064,10 +1064,7 @@ async sub mr_report ($self, $who, $arg = {}) {
 async sub post_gitlab_snippet ($self, $payload) {
   my $res = await $self->hub->http_post(
     $self->api_uri . '/v4/snippets',
-    headers => {
-      'PRIVATE-TOKEN' => $self->api_token,
-    },
-
+    'PRIVATE-TOKEN' => $self->api_token,
     Content_Type => 'application/json',
     Content      => encode_json($payload),
   );