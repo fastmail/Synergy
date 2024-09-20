aeea64280033c7150be073d2ffac6c1897045aa4
Andrew Davis <adavis@fastmailteam.com>
2024-09-20 13:06:47 +1000

    InABox: Use slack external send_file instead of api directly
    This also moves us to the new APIs, as files.upload is deprecated

--- lib/Synergy/Reactor/InABox.pm
+++ lib/Synergy/Reactor/InABox.pm
@@ -487,16 +487,8 @@ async sub _setup_droplet ($self, $event, $droplet, $key_file, $args = []) {
   if ($event->from_channel->isa('Synergy::Channel::Slack')) {
     $message .= " Here's the output from setup:";
 
-    return await $event->from_channel->slack->api_call(
-      'files.upload',
-      {
-        form_encoded => 1, # Sigh.
-
-        content => "$stderr\n----(stdout)----\n$stdout",
-        channels => $event->conversation_address,
-        initial_comment => $message,
-      },
-    );
+    my $content = "$stderr\n----(stdout)----\n$stdout";
+    await $event->from_channel->slack->send_file($event->conversation_address, 'setup.log', $content);
   } else {
     $Logger->log("we ran ssh, but not via Slack, so stdout/stderr discarded");
   }
